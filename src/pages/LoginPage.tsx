bff374437fc91f19519411ab370718c913b0d602
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2023-03-08 02:07:40 +0800

    feat: add firebase

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,34 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { provider } from "../config/firebase";
+
 export const LoginPage: React.FC = () => {
+  const onLogin = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+
+        if (credential) {
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+
+          console.log({ token, user });
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log({ errorCode, errorMessage });
+      });
+  };
+
   return (
     <div className="bg-gray-50 min-h-screen flex justify-center items-center">
-      <button className="bg-emerald-500 text-white py-4 px-8 rounded-lg hover:bg-emerald-600 transition-all duration-75">
+      <button
+        onClick={onLogin}
+        className="bg-emerald-500 text-white py-4 px-8 rounded-lg hover:bg-emerald-600 transition-all duration-75"
+      >
         Login with google
       </button>
     </div>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly APIKEY: string;
+  readonly AUTHDOMAIN: string;
+  readonly PROJECTID: string;
+  readonly STORAGEBUCKET: string;
+  readonly MESSAGINGSENDERID: string;
+  readonly APPID: string;
+  readonly MEASUREMENTID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
